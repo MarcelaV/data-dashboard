faa35c77191046ce6ad06d859836d56c07c08405
AngielGuerra <ang.guerra.g@gmail.com>
2017-11-18 17:05:41 -0300

    maquetado dashboard con img

---
+++ js/app.js
@@ -0,0 +1,9 @@
+
+//primero hacer las variables para enganchar el dom con la data y este doc con las funciones
+// hacer funcion para filtrar por sede y generacion
+// luego, hacer funcion para qaue los datos de data pasen a los garficos
+
+//hacer segunda pagina con prfiles y numeros de estudiantes
+
+var 
+//get.todas las weas() = //evento=funcion()
